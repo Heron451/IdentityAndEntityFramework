192246eaaedf75964a1cf0610bfb9979ec5cc257
Robin Hammond <robinalexander.hammond@dcmail.ca>
2020-12-18 20:04:50 -0500

    Added user Authorization to controller for the Summary and Payroll pages.

--- Controllers/EmployeePayrollController.cs
+++ Controllers/EmployeePayrollController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using IdentityAndEntityFramework.Contexts;
 using IdentityAndEntityFramework.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace IdentityAndEntityFramework.Controllers
 {
@@ -18,18 +19,18 @@ namespace IdentityAndEntityFramework.Controllers
         {
             _context = context;
         }
-
+        [Authorize]
         // GET: EmployeePayroll
         public async Task<IActionResult> Index()
         {
             return View(await _context.tblEmployeePayroll.ToListAsync());
         }
-
+        [Authorize]
         public async Task<IActionResult> Summary()
         {
             return View(await _context.tblEmployeePayroll.ToListAsync());
         }
-
+        [Authorize]
         // GET: EmployeePayroll/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -47,13 +48,13 @@ namespace IdentityAndEntityFramework.Controllers
 
             return View(employeePayrollModel);
         }
-
+        [Authorize]
         // GET: EmployeePayroll/Create
         public IActionResult Create()
         {
             return View();
         }
-
+        [Authorize]
         // POST: EmployeePayroll/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -69,7 +70,7 @@ namespace IdentityAndEntityFramework.Controllers
             }
             return View(employeePayrollModel);
         }
-
+        [Authorize]
         // GET: EmployeePayroll/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -85,7 +86,7 @@ namespace IdentityAndEntityFramework.Controllers
             }
             return View(employeePayrollModel);
         }
-
+        [Authorize]
         // POST: EmployeePayroll/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
@@ -120,7 +121,7 @@ namespace IdentityAndEntityFramework.Controllers
             }
             return View(employeePayrollModel);
         }
-
+        [Authorize]
         // GET: EmployeePayroll/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
@@ -138,7 +139,7 @@ namespace IdentityAndEntityFramework.Controllers
 
             return View(employeePayrollModel);
         }
-
+        [Authorize]
         // POST: EmployeePayroll/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
